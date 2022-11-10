8b5df1ce9d6f037f96b5632dbdec48ee8e872d88
류채연 <haha03270@gmail.com>
2022-11-10 15:52:37 +0900

    [1110] Sending data to the frontend

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const indexRouter = require("./router/index");
+
+const app = express();
+const port = 4000;
+
+app.use(cors());
+
+app.use("/", indexRouter);
+
+app.listen(port, () => {
+  console.log(`node listening on port ${port}`);
+});
